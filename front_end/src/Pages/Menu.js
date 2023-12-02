9fe9c563416a1e489b7318243404b469bd463540
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2023-12-02 21:53:02 +0300

    Menu bar
    Menu bar is done but I have to make it responsive

---
+++ front_end/src/Pages/Menu.js
@@ -0,0 +1,14 @@
+import "./Menu.css";
+import React from "react";
+//I have to fix the resposive of the menu-bar && also check the media part in css file
+function Menu() {
+  return (
+    <div>
+      <div className="menu-bar">
+        <button className="algolingo-button">AlgoLingo</button>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
