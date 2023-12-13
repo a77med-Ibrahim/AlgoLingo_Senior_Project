cace5d643cad9327a9bd505c193ad1e640207c54
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2023-12-14 00:25:07 +0300

    Menu changes
    Added Levels bar, Start Bar, and images for Stacks and Queues, and made the code more dynamic

--- front_end/src/Pages/Menu.js
+++ front_end/src/Pages/Menu.js
@@ -1,11 +1,54 @@
+import React, { useState } from "react";
 import "./Menu.css";
-import React from "react";
-//I have to fix the resposive of the menu-bar && also check the media part in css file
+
+import stackImage from "../Img/stackImg.webp";
+import queueImage from "../Img/queueImg.png";
+
 function Menu() {
+  const [activeCircle, setActiveCircle] = useState(null);
+
+  const circleData = [
+    { id: 1, top: "60px", left: "90px", isOpen: true, image: stackImage },
+    { id: 2, top: "150px", left: "-40px", isOpen: false, image: queueImage },
+    { id: 3, top: "220px", left: "-20px", isOpen: false, image: null },
+  ];
+
+  const handleCircleHover = (circleId) => {
+    setActiveCircle(circleId);
+  };
+
   return (
-    <div>
+    <div className="main-div">
       <div className="menu-bar">
         <button className="algolingo-button">AlgoLingo</button>
+      </div>
+      <div className="levels-bar">
+        <button className="levels-button">Levels</button>
+        <div className="levels-space">
+          {circleData.map((circle) => (
+            <button
+              key={circle.id}
+              className={`circles ${circle.isOpen ? "" : "closed"}`}
+              style={{ top: circle.top, left: circle.left }}
+              onMouseEnter={() => handleCircleHover(circle.id)}
+              onMouseLeave={() => setActiveCircle(null)}
+            >
+              {circle.isOpen ? circle.id : "X"}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="start-bar">
+        <div className={`image-container ${activeCircle ? "visible" : ""}`}>
+          {activeCircle && circleData[activeCircle - 1].image && (
+            <img
+              className="image"
+              src={circleData[activeCircle - 1].image}
+              alt={`Circle ${activeCircle} Image`}
+            />
+          )}
+        </div>
+        <button className="start-button">Start</button>
       </div>
     </div>
   );
