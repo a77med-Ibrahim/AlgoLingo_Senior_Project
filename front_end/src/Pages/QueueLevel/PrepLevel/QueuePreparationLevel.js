525ecd232bdf2684e3eab77c0d8f96cd4e0059cf
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2024-04-03 23:42:57 +0300

    Added buttons and the QueueBar

--- front_end/src/Pages/QueueLevel/PrepLevel/QueuePreparationLevel.js
+++ front_end/src/Pages/QueueLevel/PrepLevel/QueuePreparationLevel.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./QueuePreparationLevel.css";
 import AlgoLingoBar from "../../Menu/AlgoLingoBar";
 import LevelsBar from "./Levels Bar/LevelsBar";
-// import Amongos from "../Amongos/Amongos";
+import Amongus from "../Amongos/Amongus";
 
 function QueuePreparationLevel() {
   return (
@@ -12,7 +12,15 @@ function QueuePreparationLevel() {
         <h1 className="title-styling">Queue</h1>
         <h2 className="title-styling">Preparation</h2>
         <LevelsBar />
-        {/* <Amongos /> */}
+        <div className="queue-button-group">
+          <button>EnQueue</button>
+          <button>DeQueue</button>
+          <button>Peek</button>
+          <button>isEmpty</button>
+        </div>
+        <div className="queue-rectangle"></div>
+
+        {/* <Amongus className="amongus" /> */}
       </div>
     </div>
   );
