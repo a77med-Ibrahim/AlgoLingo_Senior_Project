bbcfc1762f9bf07cd996ed6b6a1dd9354ade50d0
Ahmed Ibrahim <105105189+ahmedIbrahim84201@users.noreply.github.com>
2024-05-09 00:04:20 +0300

    Added linkedlist lines and blocks

--- front_end/src/Pages/LinkedList/Prep/LinkedListPrepLevel.js
+++ front_end/src/Pages/LinkedList/Prep/LinkedListPrepLevel.js
@@ -1,11 +1,25 @@
 import AlgoLingoBar from "../../Menu/AlgoLingoBar";
 import LevelsBar from "../LevelBar";
+import "./LinkedListPrepLevel.css";
+
 function LinkedListPrepLevel() {
   return (
     <div>
-      <AlgoLingoBar />
-      <LevelsBar />
+      <div className="all-div">
+        <AlgoLingoBar />
+        <div className="other">
+          <h1 className="title-styling">Linked List</h1>
+          <h2 className="title-styling">Preparation</h2>
+          <div className="navbar-line" />
+          <div className="game-container">
+            <div className="linked-list-rectangles"/>
+            <div className="linked-list-rectangles"/>
+            <div className="linked-list-rectangles"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default LinkedListPrepLevel;
